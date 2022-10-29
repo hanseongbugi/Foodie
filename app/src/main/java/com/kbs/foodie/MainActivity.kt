e6e3e374f82914f7c04a390b4a17c6a10bb8216a
wannaseoji <91694379+wannaseoji@users.noreply.github.com>
2022-10-29 13:49:21 +0900

    Revert "github Test branch seo"
    This reverts commit 8d2a3d2797625ff5612ee7b776e4e8aa0015d61d.

--- app/src/main/java/com/kbs/foodie/MainActivity.kt
+++ app/src/main/java/com/kbs/foodie/MainActivity.kt
@@ -2,7 +2,7 @@ package com.kbs.foodie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-//dev seo github Testing
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
