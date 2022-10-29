33090dfc2062af72e13ab0579fab84b394cff38e
hanseongbugi <bhsung1@naver.com>
2022-10-29 13:48:43 +0900

    commit test

--- app/src/main/java/com/kbs/foodie/MainActivity.kt
+++ app/src/main/java/com/kbs/foodie/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    //dev 배한성 commit test
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
