28759c877106d2a92b830a136ca5651d0aa6bbec
hanseongbugi <bhsung1@naver.com>
2022-10-31 21:01:43 +0900

    fragment UI

--- app/src/main/java/com/kbs/foodie/HomeFragment.kt
+++ app/src/main/java/com/kbs/foodie/HomeFragment.kt
@@ -1,13 +1,13 @@
 package com.kbs.foodie
 
 import android.os.Bundle
+import android.view.Menu
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeFragment : Fragment(R.layout.home_fragment){
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-    }
 
 }
--- app/src/main/java/com/kbs/foodie/MainActivity.kt
+++ app/src/main/java/com/kbs/foodie/MainActivity.kt
@@ -2,9 +2,40 @@ package com.kbs.foodie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.*
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph) // Home이 Top 목적지
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNav)
+        bottomNavigationView?.setupWithNavController(navController)
     }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.add_menu, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.addFragment ->
+                item.onNavDestinationSelected(findNavController(R.id.fragment))
+            else -> return super.onOptionsItemSelected(item)
+        }
+        return true
+    }
+
 }
