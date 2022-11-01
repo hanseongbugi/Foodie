bb6034a63586312f913dd79fffac10c861118d10
kimsuyeon0702 <89465775+kimsuyeon0702@users.noreply.github.com>
2022-11-01 20:01:52 +0900

    loginSignUp_1

---
+++ app/src/main/java/com/kbs/foodie/LoginActivity.kt
@@ -0,0 +1,43 @@
+package com.kbs.foodie
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.kbs.foodie.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.signInButton.setOnClickListener {
+            val userEmail = binding.emailLogin.text.toString()
+            val password = binding.passwdLogin.text.toString()
+            doLogin(userEmail, password)
+        }
+        binding.signUpButton.setOnClickListener {
+            startActivity(
+                Intent(this, SignUpActivity::class.java))
+            finish()
+        }
+    }
+
+    private fun doLogin(userEmail: String, password: String) {
+        Firebase.auth.signInWithEmailAndPassword(userEmail, password)
+            .addOnCompleteListener(this) {
+                if (it.isSuccessful) {
+                    startActivity(
+                        Intent(this, MainActivity::class.java))
+                    finish()
+                } else {
+                    Log.w("LoginActivity", "signInWithEmail", it.exception)
+                    Toast.makeText(this, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+}
---
+++ app/src/main/java/com/kbs/foodie/SignUpActivity.kt
@@ -0,0 +1,48 @@
+package com.kbs.foodie
+
+import android.content.ContentValues
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.kbs.foodie.databinding.ActivitySignupBinding
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.backButton.setOnClickListener {
+            Firebase.auth.signOut()
+            startActivity(
+                Intent(this, LoginActivity::class.java)
+            )
+            finish()
+            Toast.makeText(this, "Back Button-No Auth", Toast.LENGTH_SHORT).show();
+        }
+        binding.saveButton.setOnClickListener {
+            val userEmail = binding.emailSignUpText.text.toString()
+            val password = binding.passwdSignUpText.text.toString()
+            loginUserId(userEmail, password)
+        }
+    }
+    private fun loginUserId(email:String, password: String){
+        Firebase.auth?.createUserWithEmailAndPassword(email, password)
+            ?.addOnCompleteListener(this){
+                if(it.isSuccessful){
+
+                    Log.d(ContentValues.TAG, "사용자 이메일 등록 완료");
+                    startActivity(
+                        Intent(this, LoginActivity::class.java)
+                    )
+                    finish()
+                }else{
+                    Log.d(ContentValues.TAG, "사용자 이메일 등록 실패");
+                }
+            }
+    }
+}
