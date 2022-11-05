667a67203683dc633513a36c79f86fab23cd5bbd
wannaseoji <91694379+wannaseoji@users.noreply.github.com>
2022-11-05 17:22:20 +0900

    s commit #3
    '다음','이전'기능 오류 수정
    balloon Adapter를 활용해서 text띄우기

--- app/src/main/java/com/kbs/foodie/MapFragment.kt
+++ app/src/main/java/com/kbs/foodie/MapFragment.kt
@@ -5,12 +5,15 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.kbs.foodie.databinding.MapFragmentBinding
+import net.daum.mf.map.api.CalloutBalloonAdapter
 import net.daum.mf.map.api.MapPOIItem
 import net.daum.mf.map.api.MapPoint
+import net.daum.mf.map.api.MapView
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -25,6 +28,7 @@ class MapFragment : Fragment(R.layout.map_fragment)  {
     private var pageNumber = 1      // 검색 페이지 번호
     private var keyword = ""        // 검색 키워드
     private var binding: MapFragmentBinding? = null
+    private lateinit var mapView : MapView              // 카카오 지도 뷰
     companion object {
         const val BASE_URL = "https://dapi.kakao.com/"
         const val API_KEY = "KakaoAK b4186fe9f3dc84011569230d000fc096"  // REST API 키
@@ -73,6 +77,9 @@ class MapFragment : Fragment(R.layout.map_fragment)  {
             searchKeyword(keyword, pageNumber)
         }
         Log.w("1","5")
+        mapView = binding!!.mapView   // 카카오 지도 뷰
+
+        mapView.setCalloutBalloonAdapter(CustomBalloonAdapter(layoutInflater))  // 커스텀 말풍선 등록
         return binding!!.root
 
     }
@@ -126,12 +133,31 @@ class MapFragment : Fragment(R.layout.map_fragment)  {
             }
             listAdapter.notifyDataSetChanged()
 
-           // binding?.btnNextPage?.isEnabled ?:  = !searchResult.meta.is_end // 페이지가 더 있을 경우 다음 버튼 활성화
-          //  binding?.btnPrevPage?.isEnabled ?:  = pageNumber != 1             // 1페이지가 아닐 경우 이전 버튼 활성화
+            binding?.btnNextPage?.isEnabled ?:  !searchResult.meta.is_end // 페이지가 더 있을 경우 다음 버튼 활성화
+            binding?.btnPrevPage?.isEnabled ?:   pageNumber != 1             // 1페이지가 아닐 경우 이전 버튼 활성화
 
         } else {
             // 검색 결과 없음
             Toast.makeText(context, "검색 결과가 없습니다", Toast.LENGTH_SHORT).show()
+        }
+    }
+    // 커스텀 말풍선 클래스
+    class CustomBalloonAdapter(inflater: LayoutInflater): CalloutBalloonAdapter {
+        val mCalloutBalloon: View = inflater.inflate(R.layout.balloon_layout, null)
+        val name: TextView = mCalloutBalloon.findViewById(R.id.ball_tv_name)
+        val address: TextView = mCalloutBalloon.findViewById(R.id.ball_tv_address)
+
+        override fun getCalloutBalloon(poiItem: MapPOIItem?): View {
+            // 마커 클릭 시 나오는 말풍선
+            name.text = poiItem?.itemName   // 해당 마커의 정보 이용 가능
+            address.text = "별점"
+            return mCalloutBalloon
+        }
+
+        override fun getPressedCalloutBalloon(poiItem: MapPOIItem?): View {
+            // 말풍선 클릭 시
+            address.text = "맛없어요 줠뒈 가지뫄쉐용"
+            return mCalloutBalloon
         }
     }
 
