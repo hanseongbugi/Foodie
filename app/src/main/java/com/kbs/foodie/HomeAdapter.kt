1ac4645230a28fb17a03227202878e5dd2c931a2
hanseongbugi <bhsung1@naver.com>
2022-11-12 13:48:47 +0900

    firestore gradle

---
+++ app/src/main/java/com/kbs/foodie/HomeAdapter.kt
@@ -0,0 +1,39 @@
+package com.kbs.foodie
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.kbs.foodie.databinding.HomeItemBinding
+
+class HomeAdapter:RecyclerView.Adapter<HomeAdapter.ViewHolder>() {
+    class ViewHolder(val view: View): RecyclerView.ViewHolder(view) {
+        fun setContetns(){
+            view.findViewById<ImageView>(R.id.image).setImageResource(R.drawable.ic_baseline_person_24)
+            view.findViewById<TextView>(R.id.name).text="hello"
+            view.findViewById<TextView>(R.id.address).text="seoul"
+            view.findViewById<TextView>(R.id.star).text="별점 5"
+            view.findViewById<TextView>(R.id.review).text="맛이 쓰고, 맛이 말고, 맛이 있다."
+        }
+
+
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val layoutInflater=LayoutInflater.from(parent.context)
+        val view=layoutInflater.inflate(R.layout.home_item,parent,false)
+        return ViewHolder(view)
+
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.setContetns()
+    }
+
+    override fun getItemCount(): Int {
+        return 0
+    }
+}
--- app/src/main/java/com/kbs/foodie/HomeFragment.kt
+++ app/src/main/java/com/kbs/foodie/HomeFragment.kt
@@ -3,16 +3,33 @@ package com.kbs.foodie
 import android.content.Intent
 import android.os.Bundle
 import android.view.*
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class HomeFragment : Fragment(R.layout.home_fragment) {
-
+class HomeFragment : Fragment() {
+    val adapter=HomeAdapter()
     override fun onCreate(savedInstanceState: Bundle?) {
         setHasOptionsMenu(true)
         super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val rootView = inflater.inflate(R.layout.home_fragment,container,false) as ViewGroup
+        val recyclerView=rootView.findViewById<RecyclerView>(R.id.homeView)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager=LinearLayoutManager(activity)
+        recyclerView.adapter=adapter
+        return rootView
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
